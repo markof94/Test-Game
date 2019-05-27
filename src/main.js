fb7b98e07703a1aba3c5e2fcfa7b16a62c3703a6
Marko <46076533+markof94@users.noreply.github.com>
2019-05-27 21:40:22 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -4,17 +4,27 @@
 
 *****************************************************/
 
-var stage = new PIXI.Container();
+let stage = new PIXI.Container();
+
 
 function init()
 {
-    renderer.backgroundColor = 0x22A7F0;
+
+    console.log("aaaa");
+
+    renderer.backgroundColor = 0x1111F0;
     renderer.render(stage);
+    
     loop();
+
+    
 }
 
 function loop()
 {
     requestAnimationFrame(loop);
     renderer.render(stage);
+
+   
+    
 }
